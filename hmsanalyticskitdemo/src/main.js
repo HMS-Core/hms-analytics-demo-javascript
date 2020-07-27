9a5394607b8a62441f88a2ddf7431c3a7ed8a157
iris156 <65387436+iris156@users.noreply.github.com>
2020-07-27 20:31:13 +0800

    Update main.js

--- hmsanalyticskitdemo/src/main.js
+++ hmsanalyticskitdemo/src/main.js
@@ -18,7 +18,7 @@ import App from "./App.vue";
 
 import agconnect from '@agconnect/api';
 import '@agconnect/instance';
-import '@hms/analytics';
+import '@hmscore/analytics-web';
 import router from './router.js';
 import './assets/css/global.css';
 import 'babel-polyfill';
