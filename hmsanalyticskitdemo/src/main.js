2b9b4c77aeade0fd2c460d1664ed09724cbd8b18
陈雨田 <8685024+chen12221219@user.noreply.gitee.com>
2022-09-20 23:23:53 +0800

    删除agconnect配置信息

--- hmsanalyticskitdemo/src/main.js
+++ hmsanalyticskitdemo/src/main.js
@@ -24,36 +24,7 @@ import './assets/css/global.css';
 import 'babel-polyfill';
 
 Vue.config.productionTip = false;
-var agConnectConfig = 
-{
-	"agcgw":{
-      "backurl": "https://lfagcgwtest01.hwcloudtest.cn:15602",
-      "url": "https://lfagcgwtest01cn.hwcloudtest.cn:18062"
-	},
-	"client":{
-		"cp_id":"4130086000000637176",
-		"product_id":"258913027873047688",
-		"client_id":"401147640282098944",
-		"client_secret":"ABA91A52EF1FAB651F57AF091CEC149EECAFB01757DF1224FC384D2173828480",
-		"app_id":"98751101985125555",
-		"api_key":"CgB6e3x9zs8IL+Frjllsf9DoteK4Taz7jAseM9P+ikVZBJhHu87FpdEpqq0CimpayCrJgUrkUnV2RM86fyFiD4FE"
-	},
-	"service":{
-		"analytics":{
-			"collector_url":"habackup.hwcloudtest.cn:31405,datacollector-drcn.dt.dbankcloud.cn",
-			"resource_id":"p1",
-			"channel_id":""
-		},
-		"cloudstorage":{
-			"storage_url":"https://agc-storage-drcn.platform.dbankcloud.cn"
-		},
-		"ml":{
-			"mlservice_url":"ml-api-drcn.ai.dbankcloud.com,ml-api-drcn.ai.dbankcloud.cn"
-		}
-	},
-	"region":"CN",
-	"configuration_version":"1.0"
-};
+var agConnectConfig = {};
 agconnect.instance().configInstance(agConnectConfig);
 
 Vue.prototype.$hiAnalytics = agconnect.analytics();
